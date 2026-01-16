9a4afa47f1c884106d36a75d8996f9620a464aff
咳咳 <76099203+wwkk2580@users.noreply.github.com>
2026-01-16 14:10:25 +0800

    Add timestamp to push notification content
    Append current date and time to push notification content.

--- push_channel/dingtalk_bot.py
+++ push_channel/dingtalk_bot.py
@@ -1,4 +1,5 @@
 import json
+from datetime import datetime
 
 from common import util
 from common.logger import log
@@ -13,6 +14,12 @@ class DingtalkBot(PushChannel):
             log.error(f"【推送_{self.name}】配置不完整，推送功能将无法正常使用")
 
     def push(self, title, content, jump_url=None, pic_url=None, extend_data=None):
+        # 当前日期时间
+        now_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+        # 在通知内容中追加时间
+        content_with_time = f"{content}通知时间：{now_time}"
+
         push_url = "https://oapi.dingtalk.com/robot/send"
         headers = {
             "Content-Type": "application/json"
@@ -24,7 +31,7 @@ class DingtalkBot(PushChannel):
             "msgtype": "link",
             "link": {
                 "title": title,
-                "text": content,
+                "text": content_with_time,
                 "messageUrl": jump_url
             }
         }
@@ -32,6 +39,12 @@ class DingtalkBot(PushChannel):
         if pic_url is not None:
             body["link"]["picUrl"] = pic_url
 
-        response = util.requests_post(push_url, self.name, headers=headers, params=params, data=json.dumps(body))
+        response = util.requests_post(
+            push_url,
+            self.name,
+            headers=headers,
+            params=params,
+            data=json.dumps(body)
+        )
         push_result = "成功" if util.check_response_is_ok(response) else "失败"
         log.info(f"【推送_{self.name}】{push_result}")
